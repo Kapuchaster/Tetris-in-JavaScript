34cd1a6be64b4c8dc288fb8fa8a3fc0db5bc92c7
Michal Kapuscinski <kapuchaster@gmail.com>
2018-02-19 20:08:31 +0100

    Tile creator function

--- js/blocksFactory.js
+++ js/blocksFactory.js
@@ -1,27 +1,61 @@
 "use strict"
 
-function Block () {
-  this.symbol = '';
-  this.shape = [];
-}
+var block = (function () {
 
-  let symbolArray = ['L', 'LO', 'O'];
-  let randSymbol = getRandSymbol (symbolArray);
-  let randBlock = getBlock(randSymbol);
-  return randBlock;
+  function createNewBlock (blockParams) {
+      let symbolArray = ['L', 'LO', 'O'];
+      let randSymbol = getRandSymbol (symbolArray);
+      let randBlock = getBlock (randSymbol);
+      let tile = {
+        symbol: randSymbol,
+        matrix: randBlock
+      }
+      return tile;
 
-  function getRandSymbol (symbolArray) {
-    var randNum = Math.floor((Math.random() * symbolArray.length));
+      function getRandSymbol (symbolArray) {
+        let randIndex = Math.floor((Math.random() * symbolArray.length));
+        return symbolArray[randIndex];
+      }
+
+      function getBlock(symbol) {
+        let L = [[1,0],[1,0],[1,1]];
+        let LO = [[0,1],[0,1],[1,1]];
+        let O = [[1,1],[1,1]];
+
+        switch (symbol) {
+          case 'L': return L;
+          case 'LO': return LO;
+          case 'O': return O;
+        }
+      }
   }
 
-  function getBlock(symbol) {
-    let L = [[1,0],[1,0],[1,1]];
-    let LO = [[0,1],[0,1],[1,1]];
-    let O = [[1,1],[1,1]];
+  return {createNewBlock: createNewBlock};
+})
 
-    switch (symbol) {
-      case 'L': return L;
-      case 'LO': return LO;
-      case 'O': return O;
-    }
-  }
+
+// function Block () {
+//   this.symbol = '';
+//   this.shape = [];
+// }
+//
+//   let symbolArray = ['L', 'LO', 'O'];
+//   let randSymbol = getRandSymbol (symbolArray);
+//   let randBlock = getBlock(randSymbol);
+//   return randBlock;
+//
+//   function getRandSymbol (symbolArray) {
+//     var randNum = Math.floor((Math.random() * symbolArray.length));
+//   }
+//
+//   function getBlock(symbol) {
+//     let L = [[1,0],[1,0],[1,1]];
+//     let LO = [[0,1],[0,1],[1,1]];
+//     let O = [[1,1],[1,1]];
+//
+//     switch (symbol) {
+//       case 'L': return L;
+//       case 'LO': return LO;
+//       case 'O': return O;
+//     }
+//   }
