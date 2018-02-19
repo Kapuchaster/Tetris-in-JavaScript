c75b8ac262e8a0ba50522efe7d4882c7254c9839
Michal Kapuscinski <kapuchaster@gmail.com>
2018-02-19 21:59:17 +0100

    Render Test

--- js/index.js
+++ js/index.js
@@ -8,7 +8,26 @@
 
       let testTile = blockFactory.createNewBlock();
       map.addToMap(3,3, testTile);
-      
+
+
+
+//--------------RENDER TEST-----------------
+
+  let htmlMap = document.getElementById("game-box");
+  for (let h = 0; h < map.getMap().length; h++) {
+    for (let w = 0; w < map.getMap()[0].length; w++) {
+      let div = document.createElement("div");
+      div.classList.add("block");
+      div.style.display = "inline-block";
+      if (map.getMap()[h][w]) {
+        div.style.backgroundColor = "red";
+      }
+      htmlMap.appendChild(div);
+    }
+  }
+
+//----------------Test End------------------
+
     });
   }
 )();
