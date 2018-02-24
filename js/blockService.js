c114285399781f186866b1febd0bc43dcf8976e1
Michal Kapuscinski <kapuchaster@gmail.com>
2018-02-24 16:37:55 +0100

    Implemended blockServices

--- js/blockService.js
+++ js/blockService.js
@@ -1,3 +1,26 @@
-function getRandBlock (blocks) {
-  console.log(blocks)
-}
+'use strict'
+
+var blockService = (function () {
+
+  function moveDown(block) {
+    block.position.y++;
+  }
+
+  function moveLeft(block) {
+    if (block.position.x > 0) { // + height of Block
+      block.position.x--;
+    }
+  }
+
+  function moveRight(block) {
+    if (block.position.x < 9) { // + width of Block
+      block.position.x++;
+    }
+  }
+
+  return {
+    moveDown: moveDown,
+    moveLeft: moveLeft,
+    moveRight: moveRight
+  };
+})();
